f780b5842282c9044ab4062ab14bc2c37f03516e
debojit-basak <39153154+debojit-basak@users.noreply.github.com>
2020-09-26 19:17:22 +0600

    messed up everything

---
+++ src/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhZTTDX2KfEd56PjtUR9gTrhELWebWpHE",
+    authDomain: "travel-guru-1ceb5.firebaseapp.com",
+    databaseURL: "https://travel-guru-1ceb5.firebaseio.com",
+    projectId: "travel-guru-1ceb5",
+    storageBucket: "travel-guru-1ceb5.appspot.com",
+    messagingSenderId: "524185793707",
+    appId: "1:524185793707:web:3c173f4857f103a93733ee",
+    measurementId: "G-YJZCZVPW99"
+  };
+  export default firebaseConfig;
